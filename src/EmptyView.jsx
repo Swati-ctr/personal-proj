91cef84887d92ad520b8ebd3096a4f60b551cb79
Swati-ctr <swatichoudhary106@gmail.com>
2024-08-29 12:35:39 +0530

    Update EmptyView.jsx

--- src/EmptyView.jsx
+++ src/EmptyView.jsx
@@ -1,8 +1,8 @@
 export default function EmptyView() {
   return (
     <section>
-      <h3>Empty Packing List</h3>
-      <p>Start by adding item you absolutely {"don't"} want to forget.</p>
+      <h3><b>NO PROJECTS</b></h3>
+      <p>Start by adding projects you absolutely {"don't"} want to forget.</p>
     </section>
   );
 }
