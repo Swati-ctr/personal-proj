4591d43693c6668264739ad894c0f09c7be56dce
swati chaudhary <swatichoudhary106@gmail.com>
2024-12-11 20:00:03 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <small>
-        inspired by <b>ByteGrad</b>🎀.
+        inspired by <b>ByteGrad</b>.
       </small>
       <p>
         Version <b>1.5</b>
